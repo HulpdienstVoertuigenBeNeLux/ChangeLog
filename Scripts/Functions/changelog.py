bf86322bd7cbb8de7bfdca217b78ca102f6bc54b
JR04 <175596598+0JR4@users.noreply.github.com>
2026-04-01 10:57:04 +0200

    Create changelog.py

---
+++ Scripts/Functions/changelog.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import json
+
+
+
+def update_changelog(message):
+    changelog = json.load(open(f"Changelog.json", encoding="utf8"))
+    time = datetime.today().strftime('%Y.%m.%d.%H.%M.%S')
+    changelog[time] = message
+
+    with open(f'Changelog.json', 'w+') as outfile_kaz:
+        json.dump(changelog, outfile_kaz, indent=4)
