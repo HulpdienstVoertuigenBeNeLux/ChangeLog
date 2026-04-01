4f50882e1f7c5e6f4b44d80fab3c393e6d073807
JR04 <175596598+0JR4@users.noreply.github.com>
2026-04-01 10:57:28 +0200

    Add files via upload

---
+++ Scripts/Functions/discord.py
@@ -0,0 +1,34 @@
+import requests
+import os
+
+def webhook(message):
+    color = "9212311"
+    if "added" in message:
+        color = "8388352"
+    elif "removed" in message:
+        color = "16711680"
+    elif "changed" in message:
+        color = "16746496"
+
+    webhook_url = os.getenv("DISCORD")
+    embed = {
+        "title": "Database Update",
+        "description": message,
+        "color": color,
+    }
+    webhookdata = {
+        "username": "Hulpdiensten Benelux wijziging Tracker",
+        "embeds": [
+            embed
+        ],
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    result = requests.post(webhook_url, json=webhookdata, headers=headers)
+    if 200 <= result.status_code < 300:
+        print(f"Webhook sent {result.status_code}")
+    else:
+        print(f"Not sent with {result.status_code}, response:\n{result.json()}")
